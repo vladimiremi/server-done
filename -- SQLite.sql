9848afbc3534e550bd483c592653402c16928050
vladimiremi <vladimiremi2014@gmail.com>
2020-12-28 11:00:46 -0300

    Chore: Add more data

--- -- SQLite.sql
+++ -- SQLite.sql
@@ -8,11 +8,18 @@ on produto.id = agricultor_produtos.produto_id;
 -- on produto.id = agricultor_produtos.produto_id;
 
 
+
 SELECT produto.id, produto.type, sum(agricultor_produtos.quantity) from produto join agricultor_produtos
 on produto.id = agricultor_produtos.produto_id
 where produto.type = 'Branco';
 
 select agricultor.id, agricultor.nickname, sum(agricultor_produtos.quantity) from agricultor join agricultor_produtos
+on agricultor.id = agricultor_produtos.agricultor_id
+join produto
+on produto.id = agricultor_produtos.produto_id
+where agricultor.nickname = 'Valdeci';
+
+select agricultor.id, agricultor.nickname, produto.type, agricultor_produtos.quantity from agricultor join agricultor_produtos
 on agricultor.id = agricultor_produtos.agricultor_id
 join produto
 on produto.id = agricultor_produtos.produto_id
--- src/database/seeds/create_produto.ts
+++ src/database/seeds/create_produto.ts
@@ -1,22 +1,25 @@
 import Knex from 'knex';
 
-// export async function seed(knex:Knex){
-//     await knex('produto').insert([
-//         {type: "Verdinho"},
-//         {type: "Branco"},
-//         {type: "Carioca"},
-//         {type: "Olho de Ovelha"},
-//         {type: "Coquinho"},
-//         {type: "Cojó"},
-//         {type: "Santinaço"},
-//         {type: "Bahia"},
-//         {type: "Manelão"},
-//         {type: "Lijeiro"},
-//         {type: "Baije Rocha"},
-//         {type: "Cotó"},
+export async function seed(knex:Knex){
+    await knex('produto').insert([
+        {type: "Verdinho"},
+        {type: "Branco"},
+        {type: "Carioca"},
+        {type: "Olho de Ovelha"},
+        {type: "Moitinha"},
+        {type: "Coquinho"},
+        {type: "Cojó"},
+        {type: "Santinaço"},
+        {type: "Bahia"},
+        {type: "Manelão"},
+        {type: "Lijeiro"},
+        {type: "Baije Rocha"},
+        {type: "Cotó"},
+        {type: "Casca Rocha"}
+        {type: "Canapu"}
 
-//     ])
-// }
+    ])
+}
 
 // export async function seed(knex:Knex){
 //     await knex('agricultor').insert([
@@ -27,12 +30,12 @@ import Knex from 'knex';
 //     ])
 // }
 
-export async function seed(knex:Knex){
-    await knex('agricultor_produtos').insert([
-        {quantity: 100, agricultor_id: 1, produto_id: 1},
-        {quantity: 150, agricultor_id: 2, produto_id: 2},
-        {quantity: 200, agricultor_id: 3, produto_id: 3},
-        {quantity: 400, agricultor_id: 3, produto_id: 2},
-        {quantity: 500, agricultor_id: 3, produto_id: 1},
-    ])
-}
+// export async function seed(knex:Knex){
+//     await knex('agricultor_produtos').insert([
+//         {quantity: 100, agricultor_id: 1, produto_id: 1},
+//         {quantity: 150, agricultor_id: 2, produto_id: 2},
+//         {quantity: 200, agricultor_id: 3, produto_id: 3},
+//         {quantity: 400, agricultor_id: 3, produto_id: 2},
+//         {quantity: 500, agricultor_id: 3, produto_id: 1},
+//     ])
+// }
