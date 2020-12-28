e6ca513af24160e27343fd283ce34520ffdaa96d
vladimiremi <vladimiremi2014@gmail.com>
2020-12-28 10:58:02 -0300

    Chore: Add functional routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,44 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router();
 
-routes.get('/', (requeste, response)=>{
-    return response.json({messege: 'Hello World!'});
+routes.get('/produtos', async (request, response)=>{
+    const produtos = await knex('produto').select('*');
+
+    return response.json({produtos});
+});
+
+routes.post('/agricultor', async (request, response)=>{
+    const {
+        name,
+        nickname,
+        locality,
+        city,
+        uf,
+        whatsapp,
+        password
+    } = request.body;
+
+    await knex('agricultor').insert({
+        name,
+        nickname,
+        locality,
+        city,
+        uf,
+        whatsapp,
+        password
+    });
+
+    routes.post('/produto', (requent, response)=>{
+        const {
+            quantity,
+            
+        } = request.body;
+
+    })
+
+    return response.json({success: true});
 });
 
 export default routes;
